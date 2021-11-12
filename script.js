21a2b0cd3ef0646ce7f2789507338788f457df82
taylorbruton <89506980+taylorbruton@users.noreply.github.com>
2021-11-11 19:22:32 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+function hotPink()
+{
+  let color = "#db0a81";
+  navigator.clipboard.writeText(color); 
+}
+
+function orange()
+{
+  let color = "#F56200";
+  navigator.clipboard.writeText(color); 
+}
+
+function grey()
+{
+  let color = "#333333";
+  navigator.clipboard.writeText(color); 
+}
+
+function orange2()
+{
+  let color = "#FF6E0C";
+  navigator.clipboard.writeText(color); 
+}
+
+function white()
+{
+  let color = "#FFFFFF";
+  navigator.clipboard.writeText(color); 
+}
+
+function blue()
+{
+  let color = "#24548E";
+  navigator.clipboard.writeText(color); 
+}
+
+function pink()
+{
+  let color = "#E27EB1";
+  navigator.clipboard.writeText(color); 
+}
+
+function darkBlue()
+{
+  let color = "#0F4383";
+  navigator.clipboard.writeText(color); 
+}
+
+function darkGrey()
+{
+  let color = "#1A1617";
+  navigator.clipboard.writeText(color); 
+}
+
+ <div onclick="hotPink()"> This is squaremain. </div>
+ <div onclick="orange()"> This is squaremain1. </div>
+ 
+<div onclick="grey()"> This is squareF1. </div>
+ <div onclick="orange2()"> This is squareF2. </div>
+ <div onclick="white()"> This is squareF3.' </div>
+ <div onclick="hotPink()"> This is squareF4. </div>
+
+<div onclick="blue()"> This is squareS1' </div>
+<div onclick="pink()"> This is squareS2' </div>
+<div onclick="darkBlue()"> This is squareS3' </div>
+<div onclick="darkGrey()"> This is squareS4' </div>
