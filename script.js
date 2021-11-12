6cc9bf1e0f6455d61283a1c880b33d5ddf891387
taylorbruton <89506980+taylorbruton@users.noreply.github.com>
2021-11-11 19:42:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 function hotPink()
 {
-  let color = "#db0a81";
+  let color = '#db0a81';
   navigator.clipboard.writeText(color); 
 }
 
 function orange()
 {
-  let color = "#F56200";
+  let color = '#F56200';
   navigator.clipboard.writeText(color); 
 }
 
 function grey()
 {
-  let color = "#333333";
+  let color = '#333333';
   navigator.clipboard.writeText(color); 
 }
 
 function orange2()
 {
-  let color = "#FF6E0C";
+  let color = '#FF6E0C';
   navigator.clipboard.writeText(color); 
 }
 
 function white()
 {
-  let color = "#FFFFFF";
+  let color = '#FFFFFF';
   navigator.clipboard.writeText(color); 
 }
 
 function blue()
 {
-  let color = "#24548E";
+  let color = '#24548E';
   navigator.clipboard.writeText(color); 
 }
 
 function pink()
 {
-  let color = "#E27EB1";
+  let color = '#E27EB1';
   navigator.clipboard.writeText(color); 
 }
 
 function darkBlue()
 {
-  let color = "#0F4383";
+  let color = '#0F4383';
   navigator.clipboard.writeText(color); 
 }
 
 function darkGrey()
 {
-  let color = "#1A1617";
+  let color = '#1A1617';
   navigator.clipboard.writeText(color); 
 }
 
